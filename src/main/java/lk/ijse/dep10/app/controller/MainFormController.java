e2916afa434669cfc89f322d93d265da376f5376
Gayashan_Dananjaya <gayashandananjaya3@gmail.com>
2023-06-13 01:23:22 +0530

    clrFillOnMouseExited method finalized

--- src/main/java/lk/ijse/dep10/app/controller/MainFormController.java
+++ src/main/java/lk/ijse/dep10/app/controller/MainFormController.java
@@ -375,7 +375,12 @@ public class MainFormController {
     }
 
     public void clrFillOnMouseExited(MouseEvent mouseEvent) {
-
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(0.1), clrFill);
+        scaleTransition.setFromX(1.1);
+        scaleTransition.setFromY(1.1);
+        scaleTransition.setToX(1);
+        scaleTransition.setToY(1);
+        scaleTransition.playFromStart();
     }
 
     public void clrFillOnMousePressed(MouseEvent mouseEvent) {
